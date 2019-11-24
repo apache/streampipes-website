ac1605aa2c77b5b9b77d5b89a9913e18f7de3137
Dominik Riemer <riemer@fzi.de>
2019-11-24 20:59:21 +0100

    Add Apache license headers

--- documentation/website/pages/en/versions.js
+++ documentation/website/pages/en/versions.js
@@ -1,16 +1,25 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 const React = require('react');
-
 const CompLibrary = require('../../core/CompLibrary');
-
 const Container = CompLibrary.Container;
-
 const CWD = process.cwd();
 
 const siteConfig = require(`${CWD}/siteConfig.js`);
@@ -18,7 +27,7 @@ const versions = require(`${CWD}/versions.json`);
 
 function Versions(props) {
   const latestVersion = versions[0];
-  const repoUrl = `https://github.com/streampipes/streampipes`;
+  const repoUrl = `https://github.com/apache/incubator-streampipes`;
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer versionsContainer">
