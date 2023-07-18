972a2ee70b2764e0be157e42c13cf2fea592e77f
Dominik Riemer <dominik.riemer@gmail.com>
2023-07-18 21:35:35 +0200

    Remove stackoverflow link from homepage

--- website-v2/src/components/home/Support.tsx
+++ website-v2/src/components/home/Support.tsx
@@ -6,7 +6,7 @@ const Support = (props) => (
   <>
     <PageSection sectionTitle={"Resources & Support"}
                  backgroundClass={"background-light"}>
-      <section className="d-sm-flex clearfix">
+      <section className="d-sm-flex clearfix align-items-center justify-content-center">
         <SupportLink imageSrc={"/img/open_source/icon-github.png"} href={"https://www.github.com/apache/streampipes"}
                      label={"Github"}></SupportLink>
         <SupportLink imageSrc={"/img/open_source/icon-docs.png"} href={"/docs/index.html"}
@@ -15,9 +15,6 @@ const Support = (props) => (
                      label={"Docker Hub"}></SupportLink>
         <SupportLink imageSrc={"/img/open_source/icon-pypi.png"} href={"https://pypi.org/project/streampipes/"}
                      label={"PyPi"}></SupportLink>
-        <SupportLink imageSrc={"/img/open_source/icon-stackoverflow.png"}
-                     href={"https://stackoverflow.com/questions/tagged/streampipes"}
-                     label={"Stack Overflow"}></SupportLink>
         <SupportLink imageSrc={"/img/open_source/icon-mail.png"} href={"/mailinglists.html"}
                      label={"Mailing Lists"}></SupportLink>
       </section>
