6947e2e432c698cc03815f25c8acef944c6f5200
Dominik Riemer <dominik.riemer@gmail.com>
2023-07-18 22:20:19 +0200

    Fix sidebar scrolling behaviour

--- website-v2/src/theme/DocSidebar/index.tsx
+++ website-v2/src/theme/DocSidebar/index.tsx
@@ -5,11 +5,13 @@ import DocsVersionDropdownNavbarItem from '@theme-original/NavbarItem/DocsVersio
 export default function DocSidebarWrapper(props) {
   return (
       <>
+        <div className={"scroll-custom thin-scrollbar"}>
         <div className="custom-sidebar-version">
           <div className={"custom-sidebar-inner"}><span style={{ "display": "inline-block" }}>Version</span> <DocsVersionDropdownNavbarItem dropdownItemsBefore={[]} dropdownItemsAfter={[]} /></div>
         </div>
         <div id="sidebarCssSelector">
           <DocSidebar {...props} />
+        </div>
         </div>
       </>
   );
