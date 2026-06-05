453eb0a175d873f9a3648b372e644d98f8d4498b
Dominik Riemer <dominik.riemer@gmail.com>
2026-06-05 12:41:07 +0200

    Update team

--- website-v2/src/pages/community/team.tsx
+++ website-v2/src/pages/community/team.tsx
@@ -2,7 +2,6 @@ import type { FC } from 'react';
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { translate } from '@docusaurus/Translate';
 import PageSection from '../../components/PageSection';
 import PageHeader from '../../components/PageHeader';
 
--- website-v2/src/team/team.js
+++ website-v2/src/team/team.js
@@ -1,62 +1,140 @@
 module.exports = [
   {
     name: 'Tim Bossenmaier',
-    imageSrc: '/img/about/bossenmaier.jpg'
+    imageSrc: '/img/about/bossenmaier.jpg',
+    github: 'bossenti',
+    pmc: true
   },
   {
     name: 'Christofer Dutz',
-    imageSrc: '/img/about/dutz.jpg'
+    imageSrc: '/img/about/dutz.jpg',
+    github: 'chrisdutz',
+    pmc: true
   },
   {
     name: 'Daniel Ebi',
-    imageSrc: '/img/about/ebi.png'
+    imageSrc: '/img/about/ebi.png',
+    github: 'EbiDa',
+    pmc: false
   },
   {
     name: 'Daniel Gomm',
-    imageSrc: '/img/about/gomm.jpg'
+    imageSrc: '/img/about/gomm.jpg',
+    github: 'daniel-gomm',
+    pmc: false
   },
   {
     name: 'Felix John',
-    imageSrc: '/img/about/john.jpg'
+    imageSrc: '/img/about/john.jpg',
+    github: 'Madabaru',
+    pmc: false
   },
   {
     name: 'Florian Micklich',
-    imageSrc: '/img/about/micklich.png'
+    imageSrc: '/img/about/micklich.png',
+    github: 'flomickl',
+    pmc: false
+  },
+  {
+    name: 'Stefan Obermeier',
+    imageSrc: '',
+    github: 'obermeier',
+    pmc: true
   },
   {
     name: 'Sven Oehler',
-    imageSrc: '/img/about/oehler.jpg'
+    imageSrc: '/img/about/oehler.jpg',
+    github: 'SvenO3',
+    pmc: true
+  },
+  {
+    name: 'Jean-Baptiste Onofré',
+    imageSrc: '',
+    github: 'jbonofre',
+    pmc: true
+  },
+  {
+    name: 'Justin Mclean',
+    imageSrc: '',
+    github: 'justinmclean',
+    pmc: true
+  },
+  {
+    name: 'Lukas Ott',
+    imageSrc: '',
+    github: 'ottlukas',
+    pmc: false
   },
   {
     name: 'Grainier Perera',
-    imageSrc: '/img/about/grainier.png'
+    imageSrc: '/img/about/grainier.png',
+    github: 'grainier',
+    pmc: true
   },
   {
     name: 'Dominik Riemer',
-    imageSrc: '/img/about/riemer.png'
+    imageSrc: '/img/about/riemer.png',
+    github: 'dominikriemer',
+    pmc: true
   },
   {
     name: 'Steve Yurong Su',
-    imageSrc: '/img/about/su.jpeg'
+    imageSrc: '/img/about/su.jpeg',
+    github: 'SteveYurongSu',
+    pmc: false
   },
   {
     name: 'Johannes Tex',
-    imageSrc: '/img/about/tex.png'
+    imageSrc: '/img/about/tex.png',
+    github: 'tejoha',
+    pmc: true
+  },
+  {
+    name: 'Mohanadarshan Vivekanandalingam',
+    imageSrc: '',
+    github: 'mohanvive',
+    pmc: false
   },
   {
     name: 'Xin Wang',
-    imageSrc: 'https://avatars0.githubusercontent.com/u/6711230?s=460&amp;u=b8f03cad5891dbe4eab23ca75df3d97358b23b0c&amp;v=4'
+    imageSrc: 'https://avatars0.githubusercontent.com/u/6711230?s=460&amp;u=b8f03cad5891dbe4eab23ca75df3d97358b23b0c&amp;v=4',
+    github: 'vesense',
+    pmc: true
   },
   {
     name: 'Patrick Wiener',
-    imageSrc: '/img/about/wiener.png'
+    imageSrc: '/img/about/wiener.png',
+    github: 'wipatrick',
+    pmc: true
+  },
+  {
+    name: 'Xiao Liu',
+    imageSrc: '',
+    github: 'liuxiaocs7',
+    pmc: false
+  },
+  {
+    name: 'Xinhao Liu',
+    imageSrc: '',
+    github: 'Yuukadesu',
+    pmc: false
+  },
+  {
+    name: 'Zhenyu Luo',
+    imageSrc: '',
+    github: 'luoluoyuyu',
+    pmc: true
   },
   {
     name: 'Zike Yang',
-    imageSrc: 'https://people.apache.org/~zike/me/portrait.png'
+    imageSrc: 'https://people.apache.org/~zike/me/portrait.png',
+    github: 'RobertIndie',
+    pmc: true
   },
   {
     name: 'Philipp Zehnder',
-    imageSrc: '/img/about/zehnder.png'
+    imageSrc: '/img/about/zehnder.png',
+    github: 'tenthe',
+    pmc: true
   }
 ]
